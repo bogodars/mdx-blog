adfca989bfdfc201874f38faf9a4e206d758ce5f
Bogodar Shmyglyk <bogodar@gmail.com>
2020-09-17 13:16:22 +0300

    Footer section added

---
+++ src/components/Footer.js
@@ -0,0 +1,64 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import {
+  FooterWrapper,
+  FooterSocialWrapper,
+  FooterSocialIcons,
+} from "../styles"
+
+export const Footer = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      facebook: file(relativePath: { eq: "facebook.svg" }) {
+        publicURL
+      }
+      instagram: file(relativePath: { eq: "instagram.svg" }) {
+        publicURL
+      }
+      twitter: file(relativePath: { eq: "twitter.svg" }) {
+        publicURL
+      }
+      linkedin: file(relativePath: { eq: "linkedin.svg" }) {
+        publicURL
+      }
+    }
+  `)
+
+  return (
+    <FooterWrapper>
+      <FooterSocialWrapper>
+        <FooterSocialIcons>
+          <a
+            href="https://facebook.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={data.facebook.publicURL} alt="facebook logo" />
+          </a>
+          <a
+            href="https://instagram.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={data.instagram.publicURL} alt="instagram logo" />
+          </a>
+          <a
+            href="https://twitter.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={data.twitter.publicURL} alt="twitter logo" />
+          </a>
+          <a
+            href="https://linkedin.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={data.linkedin.publicURL} alt="linkedin logo" />
+          </a>
+        </FooterSocialIcons>
+        <p>@2020 Company. All rights reserved.</p>
+      </FooterSocialWrapper>
+    </FooterWrapper>
+  )
+}
