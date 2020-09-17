964d3c7bbee01003040e90ed3398bb515ced8910
Bogodar Shmyglyk <bogodar@gmail.com>
2020-09-17 17:43:44 +0300

    Content Card React Component added

---
+++ src/components/Button.js
@@ -0,0 +1,8 @@
+import React from "react"
+import {ButtonWrapper} from "../styles"
+
+export const Button = ({children, href}) => {
+    return <ButtonWrapper to={href}>
+        {children}
+    </ButtonWrapper>
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,19 @@
 import React from "react"
-import { Container, FeatureImage } from '../components'
+import { Container, FeatureImage, Content, ContentCard } from '../components'
 
 
  const IndexPage = () => {
   return (
  <Container>
    <FeatureImage />
+   <Content>
+     <ContentCard 
+     date="22.12.2020" 
+     title="Some title" 
+     excerpt="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis, labore provident ullam consequuntur nemo vitae. Distinctio sit possimus facilis sunt ut quisquam, esse necessitatibus dolore, tempora earum minus, nesciunt et!" 
+     slug="/test"
+     />
+   </Content>
   </Container>
   )
 }
